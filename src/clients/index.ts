102ee6274a042fc5105b29b8bd09747cae983560
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2025-02-08 18:32:29 +0530

    Setup and test Telegram client

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,7 +1,5 @@
 import { AutoClientInterface } from "@elizaos/client-auto";
-import { DiscordClientInterface } from "@elizaos/client-discord";
 import { TelegramClientInterface } from "@elizaos/client-telegram";
-import { TwitterClientInterface } from "@elizaos/client-twitter";
 import { Character, IAgentRuntime } from "@elizaos/core";
 
 export async function initializeClients(
@@ -16,18 +14,9 @@ export async function initializeClients(
     if (autoClient) clients.push(autoClient);
   }
 
-  if (clientTypes.includes("discord")) {
-    clients.push(await DiscordClientInterface.start(runtime));
-  }
-
   if (clientTypes.includes("telegram")) {
     const telegramClient = await TelegramClientInterface.start(runtime);
     if (telegramClient) clients.push(telegramClient);
-  }
-
-  if (clientTypes.includes("twitter")) {
-    const twitterClients = await TwitterClientInterface.start(runtime);
-    clients.push(twitterClients);
   }
 
   if (character.plugins?.length > 0) {
